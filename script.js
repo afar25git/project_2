e2292a157d790538809e0c8a3e679a437a50559e
Andres Alfonso <afar25@gmail.com>
2022-03-28 23:16:42 -0700

    Finally figured out how to use the API chosen

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const button = document.querySelector('button')
+const getStartedButton = document.querySelector('button')
 const leagueName = document.querySelector('div')
-const leagueInput = document.querySelector('input')
 
-button.addEventListener('click', async () => {
-    let leagueOptions = leagueInput.value
+getStartedButton.addEventListener('click', async () => {
     let response = await axios.get(`https://api-football-standings.azharimm.site/leagues`)
 
     console.log(response.data.data[0].name)
-    let laLigaStandings = response.data.data[0].name
+    console.log(response.data.data[0].logos.dark)
+    console.log(response.data)
+    let laLigaStandings = response.data.data[3].name
     leagueName.innerHTML = `${laLigaStandings}`
 })
-
